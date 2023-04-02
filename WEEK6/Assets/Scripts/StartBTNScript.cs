aa8147b1a6ac3053177f34518eb7ef256550eafd
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2023-04-02 11:04:04 +0800

    -added Start Scene -added Start Button(Switch to GameScene)

---
+++ WEEK6/Assets/Scripts/StartBTNScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartBTNScript : MonoBehaviour
+{
+
+    public void start()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
