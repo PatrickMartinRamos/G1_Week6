76ff2c0405c9696e444d490b77d754b4c20cb8ec
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2023-04-02 21:00:27 +0800

    -fix enemy behavior

--- WEEK6/Assets/Scripts/EnemyBehaviorScript.cs
+++ WEEK6/Assets/Scripts/EnemyBehaviorScript.cs
@@ -6,13 +6,13 @@ public class EnemyBehaviorScript : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
-    //public GameObject impactEffect;
 
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = Vector2.down * speed;
     }
+
     void OnBecameInvisible()
     {
         Destroy(gameObject);
