6fa09e885d7a9be307eeb7e04e7dfa25ac788281
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2023-04-02 13:18:02 +0800

    -added boundary for player

--- WEEK6/Assets/Scripts/PlayerScript.cs
+++ WEEK6/Assets/Scripts/PlayerScript.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     public float speed = 15f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,8 +20,10 @@ public class PlayerScript : MonoBehaviour
         float inputhorizontal = Input.GetAxisRaw("Horizontal");
         float inputvertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(inputhorizontal, inputvertical, 0) * speed* Time.deltaTime;
+        Vector3 direction = new Vector3(inputhorizontal, inputvertical, 0) * speed * Time.deltaTime;
 
         transform.Translate(direction);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10, 10f), Mathf.Clamp(transform.position.y, -8f, 2f), transform.position.z);
     }
 }
